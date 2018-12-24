bfa584ee880eae7d0adb85a74532ebb63032b9b7
hjpcs <hejinpengcs@foxmail.com>
2018-12-24 16:18:27 +0800

    test update

--- src/main/java/com/hogwarts/App.java
+++ src/main/java/com/hogwarts/App.java
@@ -6,7 +6,17 @@ package com.hogwarts;
  */
 public class App 
 {
-    public static void main( String[] args )
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args )
     {
         System.out.println( "Hello World!" );
     }
--- src/test/java/com/hogwarts/test/Student.java
+++ src/test/java/com/hogwarts/test/Student.java
@@ -6,7 +6,50 @@ public class Student {
     int id;
     String name;
     boolean sex;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
     int age;
     String addr;
     //方法
+
+
 }
