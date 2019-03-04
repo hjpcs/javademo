5dff744dcb6c25ede71dfdd2a294771a2ce84b66
hjpcs <hejinpengcs@foxmail.com>
2019-03-04 19:03:55 +0800

    update

--- src/test/java/com/hogwarts/sql/GenerateSQL.java
+++ src/test/java/com/hogwarts/sql/GenerateSQL.java
@@ -41,41 +41,44 @@ public class GenerateSQL {
                     HSSFCell cell0 = row.getCell(0);
                     HSSFCell cell1 = row.getCell(1);
                     HSSFCell cell2 = row.getCell(2);
+                    HSSFCell cell3 = row.getCell(3);
 
-                    if (cell0 == null || cell1 == null || cell2 == null) {
+
+                    if (cell1.getStringCellValue().trim() == null || "".equals(cell1.getStringCellValue().trim()) ||
+                            cell2.getStringCellValue().trim() == null || "".equals(cell2.getStringCellValue().trim())) {
                         continue;
-                    } else if (cell0.getStringCellValue().contains("近一月")){
+                    } else if (cell0.getStringCellValue().contains("近一月") && cell3.getStringCellValue().contains("有区间")){
                         str = "select '"+cell0.getStringCellValue().trim()+"' as name, " +
-                                "max("+cell2.getStringCellValue().trim()+") as max, " +
-                                "min("+cell2.getStringCellValue().trim()+") as min " +
+                                "cast(max("+cell2.getStringCellValue().trim()+") as string) as max, " +
+                                "cast(min("+cell2.getStringCellValue().trim()+") as string) as min " +
                                 "from "+cell1.getStringCellValue().trim()+
                                 " where interval_type=1 union\n";
-                    } else if (cell0.getStringCellValue().contains("近三月")){
+                    } else if (cell0.getStringCellValue().contains("近三月") && cell3.getStringCellValue().contains("有区间")){
                         str = "select '"+cell0.getStringCellValue().trim()+"' as name, " +
-                                "max("+cell2.getStringCellValue().trim()+") as max, " +
-                                "min("+cell2.getStringCellValue().trim()+") as min " +
+                                "cast(max("+cell2.getStringCellValue().trim()+") as string) as max, " +
+                                "cast(min("+cell2.getStringCellValue().trim()+") as string) as min " +
                                 "from "+cell1.getStringCellValue().trim()+
                                 " where interval_type=2 union\n";
-                    } else if (cell0.getStringCellValue().contains("近半年")){
+                    } else if (cell0.getStringCellValue().contains("近半年") && cell3.getStringCellValue().contains("有区间")){
                         str = "select '"+cell0.getStringCellValue().trim()+"' as name, " +
-                                "max("+cell2.getStringCellValue().trim()+") as max, " +
-                                "min("+cell2.getStringCellValue().trim()+") as min " +
+                                "cast(max("+cell2.getStringCellValue().trim()+") as string) as max, " +
+                                "cast(min("+cell2.getStringCellValue().trim()+") as string) as min " +
                                 "from "+cell1.getStringCellValue().trim()+
                                 " where interval_type=3 union\n";
-                    } else if (cell0.getStringCellValue().contains("近一年")){
+                    } else if (cell0.getStringCellValue().contains("近一年") && cell3.getStringCellValue().contains("有区间")){
                         str = "select '"+cell0.getStringCellValue().trim()+"' as name, " +
-                                "max("+cell2.getStringCellValue().trim()+") as max, " +
-                                "min("+cell2.getStringCellValue().trim()+") as min " +
+                                "cast(max("+cell2.getStringCellValue().trim()+") as string) as max, " +
+                                "cast(min("+cell2.getStringCellValue().trim()+") as string) as min " +
                                 "from "+cell1.getStringCellValue().trim()+
                                 " where interval_type=4 union\n";
                     } else {
                         str = "select '"+cell0.getStringCellValue().trim()+"' as name, " +
-                                "max("+cell2.getStringCellValue().trim()+") as max, " +
-                                "min("+cell2.getStringCellValue().trim()+") as min " +
+                                "cast(max("+cell2.getStringCellValue().trim()+") as string) as max, " +
+                                "cast(min("+cell2.getStringCellValue().trim()+") as string) as min " +
                                 "from "+cell1.getStringCellValue().trim()+" union\n";
                     }
                 }
-                String path = "D:\\idea\\project\\javademo\\src\\main\\resources\\script.sql";
+                String path = "D:\\idea\\project\\javademo\\src\\main\\resources\\sql.sql";
                 writeToFile(path, str);
             }
         }
@@ -102,22 +105,23 @@ public class GenerateSQL {
                     HSSFCell cell0 = row.getCell(0);
                     HSSFCell cell1 = row.getCell(1);
                     HSSFCell cell2 = row.getCell(2);
+                    HSSFCell cell3 = row.getCell(3);
 
                     if (cell0 == null || cell1 == null || cell2 == null) {
                         continue;
-                    } else if (cell0.getStringCellValue().contains("近一月")){
+                    } else if (cell0.getStringCellValue().contains("近一月") && cell3.getStringCellValue().contains("有区间")){
                         str = "select distinct("+cell2.getStringCellValue().trim()+") as '" +
                                 cell0.getStringCellValue().trim()+"' from " +
                                 cell1.getStringCellValue().trim()+" where interval_type=1;\n";
-                    } else if (cell0.getStringCellValue().contains("近三月")){
+                    } else if (cell0.getStringCellValue().contains("近三月") && cell3.getStringCellValue().contains("有区间")){
                         str = "select distinct("+cell2.getStringCellValue().trim()+") as '" +
                                 cell0.getStringCellValue().trim()+"' from " +
                                 cell1.getStringCellValue().trim()+" where interval_type=2;\n";
-                    } else if (cell0.getStringCellValue().contains("近半年")){
+                    } else if (cell0.getStringCellValue().contains("近半年") && cell3.getStringCellValue().contains("有区间")){
                         str = "select distinct("+cell2.getStringCellValue().trim()+") as '" +
                                 cell0.getStringCellValue().trim()+"' from " +
                                 cell1.getStringCellValue().trim()+" where interval_type=3;\n";
-                    } else if (cell0.getStringCellValue().contains("近一年")){
+                    } else if (cell0.getStringCellValue().contains("近一年") && cell3.getStringCellValue().contains("有区间")){
                         str = "select distinct("+cell2.getStringCellValue().trim()+") as '" +
                                 cell0.getStringCellValue().trim()+"' from " +
                                 cell1.getStringCellValue().trim()+" where interval_type=4;\n";
@@ -128,6 +132,87 @@ public class GenerateSQL {
                     }
                 }
                 String path = "D:\\idea\\project\\javademo\\src\\main\\resources\\dict.sql";
+                writeToFile(path, str);
+            }
+        }
+    }
+
+    @Test
+    public void generateDesc() throws Exception {
+        FileInputStream fis =new FileInputStream("D:\\idea\\project\\javademo\\src\\main\\resources\\desc.xls");
+        POIFSFileSystem fs = new POIFSFileSystem(fis);
+        //创建工作簿
+        HSSFWorkbook wb = new HSSFWorkbook(fs);
+        HSSFSheet sheet = wb.getSheetAt(0);
+
+        if (sheet == null) {
+            return;
+        } else {
+            for (int i=0; i<=sheet.getLastRowNum(); i++) { //遍历行
+                String str="";
+                HSSFRow row = sheet.getRow(i);
+                if (row == null) {
+                    continue;
+                } else {
+
+                    HSSFCell cell0 = row.getCell(0);
+
+                    if (cell0 == null) {
+                        continue;
+                    } else {
+                        str = "desc "+cell0.getStringCellValue().trim()+";\n";
+                    }
+                }
+                String path = "D:\\idea\\project\\javademo\\src\\main\\resources\\desc.sql";
+                writeToFile(path, str);
+            }
+        }
+    }
+
+    @Test
+    public void generateNull() throws Exception {
+        FileInputStream fis =new FileInputStream("D:\\idea\\project\\javademo\\src\\main\\resources\\sql.xls");
+        POIFSFileSystem fs = new POIFSFileSystem(fis);
+        //创建工作簿
+        HSSFWorkbook wb = new HSSFWorkbook(fs);
+        HSSFSheet sheet = wb.getSheetAt(0);
+
+        if (sheet == null) {
+            return;
+        } else {
+            for (int i=0; i<=sheet.getLastRowNum(); i++) { //遍历行
+                String str;
+                HSSFRow row = sheet.getRow(i);
+                if (row == null) {
+                    continue;
+                } else {
+
+                    HSSFCell cell0 = row.getCell(0);
+                    HSSFCell cell1 = row.getCell(1);
+                    HSSFCell cell2 = row.getCell(2);
+                    HSSFCell cell3 = row.getCell(3);
+
+
+                    if (cell0 == null || cell1 == null || cell2 == null) {
+                        continue;
+                    } else if (cell0.getStringCellValue().contains("近一月") && cell3.getStringCellValue().contains("有区间")){
+                        str = "select * from "+cell1.getStringCellValue().trim()+
+                                " where "+cell2.getStringCellValue().trim()+" is null and interval_type=1 limit 10;\n";
+                    } else if (cell0.getStringCellValue().contains("近三月") && cell3.getStringCellValue().contains("有区间")){
+                        str = "select * from "+cell1.getStringCellValue().trim()+
+                                " where "+cell2.getStringCellValue().trim()+" is null and interval_type=2 limit 10;\n";
+                    } else if (cell0.getStringCellValue().contains("近半年") && cell3.getStringCellValue().contains("有区间")){
+                        str = "select * from "+cell1.getStringCellValue().trim()+
+                                " where "+cell2.getStringCellValue().trim()+" is null and interval_type=3 limit 10;\n";
+                    } else if (cell0.getStringCellValue().contains("近一年") && cell3.getStringCellValue().contains("有区间")){
+                        str = "select * from "+cell1.getStringCellValue().trim()+
+                                " where "+cell2.getStringCellValue().trim()+" is null and interval_type=4 limit 10;\n";
+                    } else {
+                        str = "select * from "+cell1.getStringCellValue().trim()+
+                                " where "+cell2.getStringCellValue().trim()+" is null limit 10;\n";
+                    }
+                }
+                String path = "D:\\idea\\project\\javademo\\src\\main\\resources\\null.sql";
                 writeToFile(path, str);
             }
         }
